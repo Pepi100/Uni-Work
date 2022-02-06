678d18b1667cdec14e15af32483d7d6d8e9398b8
Pepi100 <radunedelcu02@gmail.com>
2022-02-06 13:56:41 +0200

    Save

--- ANUL I/Semestrul 1/Programarea Algoritmilor/Laborator/Laborator fizic 2/cerinta 3.py
+++ ANUL I/Semestrul 1/Programarea Algoritmilor/Laborator/Laborator fizic 2/cerinta 3.py
@@ -1,19 +1,14 @@
-with open("cuburi.txt", "r") as input:
-    n = int( input.readline().strip() )
-    lista = []
-    for line in input.readlines():
-        line = line.split()
-        lista.append( (int (line[0]), line[1]) )
+v = [15, 3, 2, 10, 1, 20, 0]
+n = 7
 
-lista.sort(reverse=True)
 
-with open("turn.txt", "w") as output:
-    ultimul = ""
-    total = 0
-    for size, color in lista:
-        if color == ultimul:
-            continue
-        output.write(f"{size} {color}\n")
-        total += size
-        ultimul = color
-    output.write(f"Inaltimea totala: {total}")
+def f(*v)
+    aux = 
+
+
+
+for i in range(n):
+    print( f(i + v) )
+
+
+
