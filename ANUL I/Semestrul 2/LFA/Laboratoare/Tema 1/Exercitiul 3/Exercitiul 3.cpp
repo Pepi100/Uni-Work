d1b9484c3e9012fa6c72317317efca627eff369a
Pepi100 <radunedelcu02@gmail.com>
2022-03-02 21:29:11 +0200

    Tema 1 LFA

--- ANUL I/Semestrul 2/LFA/Laboratoare/Tema 1/Exercitiul 3/Exercitiul 3.cpp
+++ ANUL I/Semestrul 2/LFA/Laboratoare/Tema 1/Exercitiul 3/Exercitiul 3.cpp
@@ -18,15 +18,22 @@ int nrStari, nrTranzitii, stareInitiala, nrFinal, nrCuv;
 vector<vector<drum>>muchii;
 
 vector<int>stariFinale;
+vector<int>path;
+vector<int>pathAfisare;
 
 
-map<int, int> asociere;
+//aceste mapuri ma ajuta sa pot avea starile numerotate cu orice fel de numere
+//asociez fiecarei stari citita din fisier un index, numere consecutive, de la 0
+//asociereInversa este necesar doar pentru afisarea drumului
+map<int, int> asociere; 
+map<int, int> asociereInversa; 
 
 void citireStari(int n) {
 	int x;
 	for (int i = 0; i < n; i++) {
 		fin >> x;
 		asociere[x] = i;
+		asociereInversa[i] = x;
 	}
 }
 
@@ -60,21 +67,35 @@ void citireFinal(int n) {
 
 
 int verificare(char* cuv, int k, int stare) {
+	path.push_back(stare);
 	if (k == strlen(cuv)) {
-		if (find(stariFinale.begin(), stariFinale.end(), stare) != stariFinale.end())
+		if (find(stariFinale.begin(), stariFinale.end(), stare) != stariFinale.end()) {
+			pathAfisare = path;
 			return 1;
-		else return 0;
+		}
+		
+		else {
+			path.pop_back(); 
+			return 0;
+		}
 	}
 	else {
 		//cautam in vectorul starii actuale
 		int posibilitati = 0;
 
-		for (int i = 0; i < muchii[stare].size(); i++) {
+
+		for (int i = 0; i < muchii[stare].size() && posibilitati == 0; i++) {
 			if (muchii[stare][i].litera == cuv[k])
 				posibilitati += verificare(cuv, k + 1, asociere[muchii[stare][i].destinatie]);
-			if (muchii[stare][i].litera == '&')
+
+
+			if (muchii[stare][i].litera == '&' ) //am presupus ca inputul in cazul cand o muchie are valoarea lambda va fi de forma <stare> <destinatie> &
 				posibilitati += verificare(cuv, k, asociere[muchii[stare][i].destinatie]);
+
+			
 		}
+		path.pop_back();
+
 
 		//daca nu gasim nimic returnam 0
 		return posibilitati;
@@ -92,10 +113,15 @@ void rezolvare(int n) {
 		fin >> cuv;
 		fin.get();
 		int ok;
+		path.clear();
 		ok = verificare(cuv, 0, asociere[stareInitiala]);
 
-		if (ok >= 1)
-			fout << "DA" << "\n";
+		if (ok >= 1) {
+			fout << "DA ";
+			for (int i = 0; i < pathAfisare.size(); i++)fout << asociereInversa[pathAfisare[i]] << ' ';
+			fout << '\n';
+		}
+			
 		else
 			fout << "NU" << "\n";
 
