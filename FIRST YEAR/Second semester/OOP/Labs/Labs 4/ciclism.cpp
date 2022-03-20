0da543122e8f5effbbedd84c56e78a3a182d026c
Pepi100 <radunedelcu02@gmail.com>
2022-03-20 20:44:42 +0200

    30.02.2022

---
+++ FIRST YEAR/Second semester/OOP/Labs/Labs 4/ciclism.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+class InterestPoint{
+private:
+    int vizitatori;
+    float x, y;
+public:
+    InterestPoint() {}
+
+    InterestPoint( int vizitatori, float x, float y) : vizitatori(vizitatori), x(x), y(y) {}
+
+    float getDistance(InterestPoint p2){
+        float d1 = abs(x - p2.x), d2= abs(y - p2.y);
+        return sqrt(d1*d1+d2*d2);
+    }
+
+    int getVizitatori(){
+        return vizitatori;
+    }
+
+
+    friend ostream &operator<<(ostream &os, const InterestPoint &punct);
+    friend istream &operator>>(istream &is, InterestPoint &punct);
+};
+
+ostream &operator<<(ostream &os, const InterestPoint &punct){
+    os<<" Viziatori: "<<punct.vizitatori<<" Coordonate: "<< punct.x<<", "<<punct.y<<"\n";
+    return os;
+}
+istream &operator>>(istream &is, InterestPoint &punct){
+    //cout<<"Numar vizitatori: ";
+    is >> punct.vizitatori;
+    //cout<<"Coordonate: ";
+    is>>punct.x>>punct.y;
+    return is;
+}
+
+
+class BikeTrail{
+private:
+    string nume;
+    string oras;
+    vector<InterestPoint> puncte;
+
+public:
+    BikeTrail() {}
+
+    BikeTrail(const string &nume, const string &oras, const vector<InterestPoint> &puncte) : nume(nume), oras(oras),
+                                                                                             puncte(puncte) {}
+    friend ostream &operator<<(ostream &os, const BikeTrail &trail);
+    friend istream &operator>>(istream &is, BikeTrail &trail);
+
+     string getNume() const {
+        return nume;
+    }
+
+    void setNume(const string _nume) {
+        nume = _nume;
+    }
+
+     string getOras() const {
+        return oras;
+    }
+
+    void setOras(const string _oras) {
+        oras = _oras;
+    }
+
+    vector<InterestPoint> getPuncte(){
+         return puncte;
+     }
+
+
+};
+ostream &operator<<(ostream &os, const BikeTrail &trail){
+    os<<"Nume: "<< trail.nume<<" Oras: "<<trail.oras<<"\n";
+    os<<"Puncte interes: \n";
+    for(int i=0; i< trail.puncte.size(); i++){
+        os<<trail.puncte[i]<<" ";
+    }
+
+    return os;
+}
+
+
+istream &operator>>(istream &is, BikeTrail &trail){
+    is>> trail.nume>>trail.oras;
+    int nr;
+    is>>nr;
+    InterestPoint ip;
+    for(int i=1; i<=nr; i++){
+        cin>>ip;
+        trail.puncte.push_back(ip);
+    }
+
+
+    return is;
+
+}
+
+void rezolvare(BikeTrail trail){
+    int a, b, c;
+    vector<InterestPoint> v = trail.getPuncte();
+    int maxim = 0;
+    for(int i = 0; i<trail.getPuncte().size();i++){
+        for(int j = 0; j<trail.getPuncte().size();j++){
+            for(int k = 0; k<trail.getPuncte().size();k++){
+                if( i!=j && j!=k && i!=k && v[i].getDistance(v[j])+v[j].getDistance(v[k]) > maxim ){
+                    a=i,b=j,c=k;
+                    maxim = v[i].getDistance(v[j])+v[j].getDistance(v[k]);
+                }
+            }
+        }
+
+    }
+
+    cout<<"Distanta maxima dintre 3 puncte: "<<maxim<<"\n Punctele:\n";
+    cout<<v[a]<<v[b]<<v[c];
+    cout<<"Suma vizitatorilor: "<<v[a].getVizitatori()+v[b].getVizitatori()+v[c].getVizitatori()<<'\n';
+
+}
+
+
+void rezolvare1(BikeTrail trail){
+    int a, b, c;
+    vector<InterestPoint> v = trail.getPuncte();
+    int vMaxim = -1;
+    int dmin;
+    for(int i = 0; i<trail.getPuncte().size();i++){
+        for(int j = 0; j<trail.getPuncte().size();j++){
+            for(int k = 0; k<trail.getPuncte().size();k++){
+                if( i!=j && j!=k && i!=k && v[i].getVizitatori()+v[j].getVizitatori()+v[k].getVizitatori() > vMaxim){
+                    vMaxim = v[i].getVizitatori()+v[j].getVizitatori()+v[k].getVizitatori();
+                    dmin = v[i].getDistance(v[j])+v[j].getDistance(v[k]);
+                    a=i,b=j,c=k;
+                    if(v[i].getDistance(v[k])+v[k].getDistance(v[j]) < dmin ){
+                        a=i,b=k,c=j;
+                        dmin = v[i].getDistance(v[k])+v[k].getDistance(v[j]);
+                    }
+                    if(v[j].getDistance(v[i])+v[i].getDistance(v[k]) < dmin ){
+                        a=j,b=i,c=k;
+                        dmin = v[i].getDistance(v[k])+v[k].getDistance(v[j]);
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    cout<<"Numarul maxim de vizitatori dintre 3 puncte: "<<vMaxim<<"\n Punctele:\n";
+    cout<<v[a]<<v[b]<<v[c];
+
+}
+
+
+
+
+int main() {
+    BikeTrail b;
+    cin>>b;
+    rezolvare1(b);
+
+}
+
+
+/*
+Traseu#1
+Constanta
+5
+102  0 0
+404  10 10
+3    1 1
+22   3 2
+1001 5 0
+*/
+
--- FIRST YEAR/Second semester/OOP/Labs/Labs 4/main.cpp
+++ FIRST YEAR/Second semester/OOP/Labs/Labs 4/main.cpp
@@ -1,142 +1,284 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
 
 
-class InterestPoint{
+
+int min(int x1, int x2, int x3, int x4, int x5 ){
+    int a1 = min(x1, x2), a2=min(x3,x4);
+    a1=min(a1, a2);
+    return min(a1, x5);
+}
+
+class Coords{
 private:
-    int vizitatori;
-    float x, y;
+    int x, y;
 public:
-    InterestPoint() {}
-
-    InterestPoint( int vizitatori, float x, float y) : vizitatori(vizitatori), x(x), y(y) {}
-
-    float getDistance(InterestPoint p2){
-        float d1 = abs(x - p2.x), d2= abs(y - p2.y);
-        return sqrt(d1*d1+d2*d2);
+    Coords(){}
+    Coords(int _x, int _y){
+        x=_x;
+        y=_y;
     }
 
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
 
-    friend ostream &operator<<(ostream &os, const InterestPoint &punct);
-    friend istream &operator>>(istream &is, InterestPoint &punct);
+    void setX(int _x){
+        x=_x;
+    }
+
+    void setY(int _y){
+        y=_y;
+    }
+
+    friend istream &operator>>(istream &is, Coords &coord);
+    friend ostream &operator<<(ostream &os, Coords &coord);
+
 };
 
-ostream &operator<<(ostream &os, const InterestPoint &punct){
-    os<<" Viziatori: "<<punct.vizitatori<<" Coordonate: "<< punct.x<<", "<<punct.y<<"\n";
-    return os;
-}
-istream &operator>>(istream &is, InterestPoint &punct){
-    //cout<<"Numar vizitatori: ";
-    is >> punct.vizitatori;
-    //cout<<"Coordonate: ";
-    is>>punct.x>>punct.y;
-    return is;
-}
-
-
-class BikeTrail{
-private:
-    string nume;
-    string oras;
-    vector<InterestPoint> puncte;
-
-public:
-    BikeTrail() {}
-
-    BikeTrail(const string &nume, const string &oras, const vector<InterestPoint> &puncte) : nume(nume), oras(oras),
-                                                                                             puncte(puncte) {}
-    friend ostream &operator<<(ostream &os, const BikeTrail &trail);
-    friend istream &operator>>(istream &is, BikeTrail &trail);
-
-     string getNume() const {
-        return nume;
-    }
-
-    void setNume(const string _nume) {
-        nume = _nume;
-    }
-
-     string getOras() const {
-        return oras;
-    }
-
-    void setOras(const string _oras) {
-        oras = _oras;
-    }
-
-    vector<InterestPoint> getPuncte(){
-         return puncte;
-     }
-
-
-};
-ostream &operator<<(ostream &os, const BikeTrail &trail){
-    os<<"Nume: "<< trail.nume<<" Oras: "<<trail.oras<<"\n";
-    os<<"Puncte interes: \n";
-    for(int i=0; i< trail.puncte.size(); i++){
-        os<<trail.puncte[i]<<" ";
-    }
-
-    return os;
-}
-
-
-istream &operator>>(istream &is, BikeTrail &trail){
-    is>> trail.nume>>trail.oras;
-    int nr;
-    is>>nr;
-    InterestPoint ip;
-    for(int i=1; i<=nr; i++){
-        cin>>ip;
-        trail.puncte.push_back(ip);
-    }
-
+istream &operator>>(istream &is, Coords &coord){
+    cout<<"x: ";
+    is>>coord.x;
+    cout<<"y: ";
+    is>>coord.y;
 
     return is;
 
 }
 
-void rezolvare(BikeTrail trail){
-    int a, b, c;
-    vector<InterestPoint> v = trail.getPuncte();
-    int maxim = 0;
-    for(int i = 0; i<trail.getPuncte().size();i++){
-        for(int j = 0; j<trail.getPuncte().size();j++){
-            for(int k = 0; k<trail.getPuncte().size();k++){
-                if( i!=j && j!=k && i!=k && v[i].getDistance(v[j])+v[j].getDistance(v[k]) > maxim ){
-                    a=i,b=j,c=k;
-                    maxim = v[i].getDistance(v[j])+v[j].getDistance(v[k]);
-                }
+ostream &operator<<(ostream &os, Coords &coord){
+    cout<<"x: ";
+    os<<coord.x;
+    cout<<"\ny: ";
+    os<<coord.y;
+    cout<<'\n';
+
+    return os;
+}
+
+class Queue{
+private:
+    vector<Coords> coada;
+
+public:
+    Queue(){
+        vector<Coords> c;
+        coada = c;
+    }
+
+    Queue(const vector<Coords> &coada) : coada(coada) {}
+
+    void setCoada(const vector<Coords> &coada) {
+        Queue::coada = coada;
+    }
+
+    const vector<Coords> &getCoada() const {
+        return coada;
+    }
+
+    void clear(){
+        vector<Coords> c;
+        setCoada(c);
+    }
+
+    bool isEmpty(){
+        return coada.size()==0;
+    }
+
+    int size(){
+        return coada.size();
+    }
+
+    Coords operator[](int i){
+        if(i>=0 && i<coada.size()){
+            return coada[i];
+        }else{
+            cout<<"Nu exista un element la acea pozitie in coada\n";
+            return Coords(-1,-1);
+        }
+    }
+
+    Queue operator+=(Coords _new){
+        Queue q1(coada);
+        q1.coada.push_back(_new);
+        setCoada(q1.coada);
+
+        return q1;
+    }
+
+    Queue operator--(){
+        Queue q1(coada);
+        q1.coada.erase(q1.coada.begin());
+        setCoada(q1.coada);
+        return q1;
+    }
+
+    bool exista(Coords c){
+        for(int i=0; i< coada.size();i++)
+            if(coada[i].getX()==c.getX() && coada[i].getY() == c.getY())
+                return true;
+        return false;
+    }
+
+
+friend istream &operator>>(istream &is, Queue &queue);
+};
+
+istream &operator>>(istream &is, Queue &queue){
+    int n;
+    Coords x;
+    is>>n;
+
+    for(int i=0; i<n;i++){
+        is>>x;
+        queue+=x;
+    }
+    return is;
+}
+
+class Maze{
+private:
+    int n;
+    Coords start, end;
+    Queue obstacole;
+public:
+    Maze() = default;
+
+    Maze(int n, const Coords &start, const Coords &anEnd, const Queue &obstacole) : n(n), start(start), end(anEnd),
+                                                                                    obstacole(obstacole) {}
+    int getN() const {
+        return n;
+    }
+
+    const Coords &getStart() const {
+        return start;
+    }
+
+    const Coords &getAnEnd() const {
+        return end;
+    }
+
+    const Queue &getObstacole() const {
+        return obstacole;
+    }
+
+    friend istream &operator>>(istream &is, Maze &m);
+    friend ostream &operator<<(ostream &os, Maze &m);
+
+    void fill(){
+        int matrix[101][101];
+        for(int i=0; i<=n;i++){
+            for(int j=0; j<=n;j++){
+                matrix[i][j]=INT_MAX;
             }
         }
+        Queue q;
+        q+=start;
+        matrix[start.getY()][start.getX()] = 0;
+
+        while(q.size() != 0){
+
+
+
+        }
+
 
     }
 
-    cout<<"Distanta maxima dintre 3 puncte: "<<maxim<<"\n Punctele:\n";
-    cout<<v[a]<<v[b]<<v[c];
 
+
+};
+
+istream &operator>>(istream &is, Maze &m) {
+    cout<<"n=";
+    is>>m.n;
+    cout<<"Start:\n";
+    is>>m.start;
+    cout<<"End:\n";
+    is>>m.end;
+    cout<<"Queue:\n";
+    is>>m.obstacole;
+
+    return is;
+
+}
+
+ostream &operator<<(ostream &os, Maze &m){
+    cout<<"Matricea asociata maze-ului: \n";
+    int matrix[101][101];
+
+    //totul liber;
+    for(int i=0; i<m.n;i++){
+        for(int j=0; j<m.n;j++){
+            matrix[i][j]=1;
+        }
+    }
+
+    //start si end
+    // am observat din exemplu ca y este pentru linie si x pentru coloana
+
+    matrix[m.start.getY()][m.start.getX()] = -2;
+    matrix[m.end.getY()][m.end.getX()] = -3;
+
+    //obstacolele
+//    cout<<m.obstacole.size();
+
+    for(int i=0; i< m.obstacole.size();i++){
+        matrix[m.obstacole[i].getY()][m.obstacole[i].getX()] = 0;
+    }
+
+    //afisare matrice
+
+    for(int i=0; i<m.n;i++){
+        for(int j=0; j<m.n;j++){
+            if(matrix[i][j]<0){
+                if(matrix[i][j] == -2) cout<<"s ";
+                else cout<<"e ";
+            }else{
+                cout<<matrix[i][j]<<" ";
+            }
+        }
+        cout<<'\n';
+    }
+
+
+    return os;
 }
 
 
 
-int main() {
-    BikeTrail b;
-    cin>>b;
-    rezolvare(b);
+int main(){
+    Maze m;
+    cin>>m;
+    cout<<m;
 
+
+
+    return 0;
 }
 
 
 /*
-Traseu#1
-Constanta
-5
-102  0 0
-404  10 10
-3    1 1
-22   3 2
-1001 5 0
-*/
 
+5
+0
+0
+4
+0
+5
+1
+0
+1
+2
+2
+2
+3
+2
+4
+1
+*/
