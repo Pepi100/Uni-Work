8a442a4fef98e2062615b9321487b7cc898ba89c
Pepi100 <radunedelcu02@gmail.com>
2022-04-14 15:03:27 +0300

    Update 14.04.2022

---
+++ FIRST YEAR/Second semester/FLA/Laboratoare/Tema 2/cmake-build-debug/CMakeFiles/Tema2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema2.dir/main.cpp.obj"
+  "CMakeFiles/Tema2.dir/main.cpp.obj.d"
+  "Tema2.exe"
+  "Tema2.exe.manifest"
+  "Tema2.pdb"
+  "libTema2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
