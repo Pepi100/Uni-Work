86f065e2c4ab91bee1cc8bbb62f913a9e565dee5
Pepi100 <radunedelcu02@gmail.com>
2023-05-26 23:59:48 +0300

    Database Connection

--- SECOND YEAR/Second semester/Advanced Object Oriended Programming/Project/src/Main.java
+++ SECOND YEAR/Second semester/Advanced Object Oriended Programming/Project/src/Main.java
@@ -10,6 +10,12 @@ public class Main {
             Connection con=DriverManager.getConnection(
                     "jdbc:oracle:thin:@localhost:1521:xe","test","test1234");
 
+            String query = "INSERT INTO address ( city,country, street, \"NUMBER\") VALUES ( 'Bucuresti','Romania','Alexandru Macedon', 29)";
+            Statement pstmt = con.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery(query);
+
+
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
